f6d9bd814ec87f5a7420607a57243d08bb0280cb
JohnWebb00 <90259425+JohnWebb00@users.noreply.github.com>
2022-04-14 20:09:15 +0200

    Add app home screen

--- SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/MainActivity.java
+++ SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.setTitle("Welcome");
+
         if(getSupportActionBar() != null){
             getSupportActionBar().hide();
         }
