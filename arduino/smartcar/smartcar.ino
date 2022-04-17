cf1f3c16068ba691cba40c18a529167841e0d1b7
Lance J <lancear153@gmail.com>
2022-04-17 21:26:26 +0200

    Fix arduino merge duplicates

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -66,19 +66,6 @@ DirectionlessOdometer rightOdometer(
 // For the camera
 std::vector<char> frameBuffer;
 
-ArduinoRuntime arduinoRuntime;
-BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
-BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
-DifferentialControl control(leftMotor, rightMotor);
-
-SimpleCar car(control);
-
-const int triggerPin           = 6; // D6
-const int echoPin              = 7; // D7
-const unsigned int maxDistance = 100;
-
-SR04 front{arduinoRuntime, triggerPin, echoPin, maxDistance};
-
 void setup() {
   Serial.begin(9600);
 
