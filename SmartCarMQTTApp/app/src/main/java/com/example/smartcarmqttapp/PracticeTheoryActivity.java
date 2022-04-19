7dc3af74c83f1afba94e2e7ce569257b546c4f9e
bardiaf <bardiaf@student.chalmers.se>
2022-04-19 15:21:56 +0200

    #10 Add navigation to all screens

---
+++ SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/PracticeTheoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartcarmqttapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticeTheoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice_theory);
+    }
+}
