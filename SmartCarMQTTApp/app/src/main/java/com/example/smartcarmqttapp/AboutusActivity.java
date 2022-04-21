4b202ee05046af54b7cc1af778d1c9461cdb5b0a
bardiaForooraghi <bardiaf@student.chalmers.se>
2022-04-21 13:10:45 +0200

    #10 Move bottom naviagtion bar to bottom

--- SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/AboutusActivity.java
+++ SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/AboutusActivity.java
@@ -1,18 +1,56 @@
 package com.example.smartcarmqttapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 
-public class AboutusActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_aboutus);
+        setContentView(R.layout.activity_about_us);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.aboutUs);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.connectedCar:
+                        startActivity(new Intent(getApplicationContext(), ConnectedCarActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.practiceDriving:
+                        startActivity(new Intent(getApplicationContext(), PracticeDrivingActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.practiceTheory:
+                        startActivity(new Intent(getApplicationContext(), PracticeTheoryActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.aboutUs:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void openGitHub(View view){
