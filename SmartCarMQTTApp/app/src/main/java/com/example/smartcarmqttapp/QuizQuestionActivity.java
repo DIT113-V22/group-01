21d97e417def99e56290735f5b908d45b8430d0d
GeorgZs <georg.zsolnai123@gmail.com>
2022-04-25 23:41:24 +0200

    #37 create basic ui for question screen

--- SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/QuizQuestionActivity.java
+++ SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/QuizQuestionActivity.java
@@ -6,24 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class PracticeTheoryActivity extends AppCompatActivity {
+public class QuizQuestionActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_practice_theory);
-        goToQuiz();
+        setContentView(R.layout.activity_quiz_question);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.practiceTheory);
 
+        //TODO for @Lancear: Add a listener/if statement that essentially asks the user whether they are sure
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -55,16 +54,4 @@ public class PracticeTheoryActivity extends AppCompatActivity {
             }
         });
     }
-
-    public void goToQuiz(){
-        Button button = findViewById(R.id.goToQuiz);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //go to quiz screen
-                startActivity(new Intent(PracticeTheoryActivity.this, QuizQuestionActivity.class));
-            }
-        });
-    }
-
 }
