7bfe24479a96bafba0a345797d18411834514c2b
JohnWebb00 <90259425+JohnWebb00@users.noreply.github.com>
2022-04-28 18:52:02 +0200

    #35 Update QuizContract class and db class. Add methods for creating the questions and adding them to the table

--- SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/CrushersDataBase.java
+++ SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/CrushersDataBase.java
@@ -1,25 +1,20 @@
 package com.example.smartcarmqttapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.example.smartcarmqttapp.QuizContract.*;
 
 import androidx.annotation.Nullable;
 
 public class CrushersDataBase extends SQLiteOpenHelper {
+    private SQLiteDatabase db;
+
     private Context context;
     private static final String DATABASE_NAME = "CrushersDataBase.db";
     private static final int DATABASE_VERSION = 1;
 
-    private static final String TABLE_NAME = "question_table";
-    private static final String COLUMN_QID = "qid";
-    private static final String COLUMN_QUESTION = "question";
-    private static final String COLUMN_ANSWER1 = "ans_1";
-    private static final String COLUMN_ANSWER2 = "ans_2";
-    private static final String COLUMN_ANSWER3 = "ans_3";
-    private static final String COLUMN_ANSWER4 = "ans_4";
-    private static final int COLUMN_CORRECT_ANSWER = 0;
-    private static  final String COLUMN_CATEGORY = "category";
 
     public CrushersDataBase(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -27,24 +22,45 @@ public class CrushersDataBase extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-    String query = "CREATE TABLE " + TABLE_NAME +
-            " (" + COLUMN_QID + " INTEGER PRIMARY KEY, " +
-            COLUMN_QUESTION + " TEXT," +
-            COLUMN_ANSWER1 + " TEXT, " +
-            COLUMN_ANSWER2 + " TEXT, " +
-            COLUMN_ANSWER3 + " TEXT, " +
-            COLUMN_ANSWER4 + " TEXT, " +
-            COLUMN_CORRECT_ANSWER + " TEXT," +
-            COLUMN_CATEGORY + " TEXT);";
-    sqLiteDatabase.execSQL(query);
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+        final String createTableQuery = "CREATE TABLE " + QuestionsTable.TABLE_NAME+
+            " (" + QuestionsTable._ID + " INTEGER PRIMARY KEY, " +
+            QuestionsTable.COLUMN_QUESTION + " TEXT," +
+            QuestionsTable.COLUMN_ANSWER1 + " TEXT, " +
+            QuestionsTable.COLUMN_ANSWER2 + " TEXT, " +
+            QuestionsTable.COLUMN_ANSWER3 + " TEXT, " +
+            QuestionsTable.COLUMN_ANSWER4 + " TEXT, " +
+            QuestionsTable.COLUMN_CORRECT_ANSWER + " INTEGER," +
+            QuestionsTable.COLUMN_CATEGORY + " TEXT);";
+    db.execSQL(createTableQuery);
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-    sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-    onCreate(sqLiteDatabase);
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+    db.execSQL("DROP TABLE IF EXISTS " + QuestionsTable.TABLE_NAME);
+    onCreate(db);
+    }
+
+    private void populateQuestionsTable(Question question){
+        //Place holder question values for now
+        Question q1 = new Question("Question","ans1","ans2","ans3","ans4",3,"Safety");
+        populateQuestionsTable(q1);
+    }
+
+    private void createQuestion(Question question){
+        ContentValues cv = new ContentValues();
+        cv.put(QuestionsTable.COLUMN_QUESTION, question.getQuestion());
+        cv.put(QuestionsTable.COLUMN_ANSWER1, question.getAnswer_1());
+        cv.put(QuestionsTable.COLUMN_ANSWER2, question.getAnswer_2());
+        cv.put(QuestionsTable.COLUMN_ANSWER3, question.getAnswer_3());
+        cv.put(QuestionsTable.COLUMN_ANSWER4, question.getAnswer_4());
+        cv.put(QuestionsTable.COLUMN_CORRECT_ANSWER, question.getCorrect_Answer());
+        cv.put(QuestionsTable.COLUMN_CATEGORY, question.getCategory());
+        db.insert(QuestionsTable.TABLE_NAME, null, cv);
+
+
     }
 
 
--- SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/QuizContract.java
+++ SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/QuizContract.java
@@ -1,4 +1,19 @@
 package com.example.smartcarmqttapp;
 
-public class QuizContract {
+import android.provider.BaseColumns;
+
+public final class QuizContract {
+    public QuizContract(){
+    }
+
+    public static class QuestionsTable implements BaseColumns {
+        public static final String TABLE_NAME = "question_table";
+        public static final String COLUMN_QUESTION = "question";
+        public static final String COLUMN_ANSWER1 = "ans_1";
+        public static final String COLUMN_ANSWER2 = "ans_2";
+        public static final String COLUMN_ANSWER3 = "ans_3";
+        public static final String COLUMN_ANSWER4 = "ans_4";
+        public static final String COLUMN_CORRECT_ANSWER = "answer_number";
+        public static  final String COLUMN_CATEGORY = "category";
+    }
 }
