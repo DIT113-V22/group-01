7916e0ed2e689502bf715185309daf7f380f5a4c
GeorgZs <georg.zsolnai123@gmail.com>
2022-05-03 23:04:34 +0200

    #37 Remove commented code, fix timer constant

--- SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/PracticeTheoryActivity.java
+++ SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/PracticeTheoryActivity.java
@@ -34,7 +34,7 @@ public class PracticeTheoryActivity extends AppCompatActivity {
     private int questionCount = 0;
 
     //Countdown timer
-    public static final int TEN_MIN_IN_MILLIS = 10000;
+    public static final int TEN_MIN_IN_MILLIS = 600000;
     public static final int FIFTEEN_MIN_IN_MILLIS = 900000;
     public static final int TWENTY_MIN_IN_MILLIS = 1200000;
     private TextView timer;
@@ -116,22 +116,6 @@ public class PracticeTheoryActivity extends AppCompatActivity {
 
             }
         });
-
-        /*
-            @Override
-            public void onClick(View view) {
-                if(enableTimer.isChecked()) {
-                    timerDialog.setContentView(R.layout.timer_dialog);
-                    timerDialog.show();
-                }
-                else {
-                    timerDialog.cancel();
-                    timer.setText("00:00");
-                }
-            }
-        });
-
-         */
 
         settingsDialog = new Dialog(this);
         settingsButton = findViewById(R.id.settingsImage);
