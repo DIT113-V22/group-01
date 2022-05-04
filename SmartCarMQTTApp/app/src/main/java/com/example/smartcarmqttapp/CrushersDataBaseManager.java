88a769865b161c3f7ce2f97bb92dad76cffd7aef
bardiaForooraghi <bardiaf@student.chalmers.se>
2022-05-04 17:37:36 +0200

    # 39 Add more queries for reading specific records

--- SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/CrushersDataBaseManager.java
+++ SmartCarMQTTApp/app/src/main/java/com/example/smartcarmqttapp/CrushersDataBaseManager.java
@@ -57,9 +57,99 @@ public class CrushersDataBaseManager {
         return results;
     }
 
-    public Cursor fetch() {
+    private Cursor fetch() {
         String[] columns = new String[] {CrushersDataBase.COLUMN_ID, CrushersDataBase.COLUMN_SCORE, CrushersDataBase.COLUMN_CORRECT_ANSWERS, CrushersDataBase.COLUMN_WRONG_ANSWERS, CrushersDataBase.COLUMN_CATEGORY};
         Cursor cursor = database.query(CrushersDataBase.TABLE_NAME, columns, null, null, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        return cursor;
+    }
+
+    public List<Result> getBasicTrafficRulesCategory() {
+        List<Result> results = new ArrayList<>();
+        database = crushersDataBase.getReadableDatabase();
+        Cursor cursor = this.fetchBasicTrafficRulesCategoryRecords();
+
+        if (cursor.moveToFirst()) {
+            do {
+                Result result = new Result();
+                result.setScore(cursor.getInt(cursor.getColumnIndexOrThrow(CrushersDataBase.COLUMN_SCORE)));
+                result.setNumOfCorrectAnswers(cursor.getInt(cursor.getColumnIndexOrThrow(CrushersDataBase.COLUMN_CORRECT_ANSWERS)));
+                result.setNumOfWrongAnswers(cursor.getInt(cursor.getColumnIndexOrThrow(CrushersDataBase.COLUMN_WRONG_ANSWERS)));
+                result.setCategory(cursor.getString(cursor.getColumnIndexOrThrow(CrushersDataBase.COLUMN_CATEGORY)));
+                results.add(result);
+            } while (cursor.moveToNext());
+        }
+
+        return results;
+    }
+
+    private Cursor fetchBasicTrafficRulesCategoryRecords() {
+        String category = "Basic Traffic Rules and Signs";
+        String[] columns = new String[] {CrushersDataBase.COLUMN_ID, CrushersDataBase.COLUMN_SCORE, CrushersDataBase.COLUMN_CORRECT_ANSWERS, CrushersDataBase.COLUMN_WRONG_ANSWERS, CrushersDataBase.COLUMN_CATEGORY};
+        Cursor cursor = database.query(CrushersDataBase.TABLE_NAME, columns, CrushersDataBase.COLUMN_CATEGORY + " = '" + category + "'", null, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        return cursor;
+    }
+
+    public List<Result> getEnvironmentCategory() {
+        List<Result> results = new ArrayList<>();
+        database = crushersDataBase.getReadableDatabase();
+        Cursor cursor = this.fetchEnvironmentCategoryRecords();
+
+        if (cursor.moveToFirst()) {
+            do {
+                Result result = new Result();
+                result.setScore(cursor.getInt(cursor.getColumnIndexOrThrow(CrushersDataBase.COLUMN_SCORE)));
+                result.setNumOfCorrectAnswers(cursor.getInt(cursor.getColumnIndexOrThrow(CrushersDataBase.COLUMN_CORRECT_ANSWERS)));
+                result.setNumOfWrongAnswers(cursor.getInt(cursor.getColumnIndexOrThrow(CrushersDataBase.COLUMN_WRONG_ANSWERS)));
+                result.setCategory(cursor.getString(cursor.getColumnIndexOrThrow(CrushersDataBase.COLUMN_CATEGORY)));
+                results.add(result);
+            } while (cursor.moveToNext());
+        }
+
+        return results;
+    }
+
+    private Cursor fetchEnvironmentCategoryRecords() {
+        String category = "Environment";
+        String[] columns = new String[] {CrushersDataBase.COLUMN_ID, CrushersDataBase.COLUMN_SCORE, CrushersDataBase.COLUMN_CORRECT_ANSWERS, CrushersDataBase.COLUMN_WRONG_ANSWERS, CrushersDataBase.COLUMN_CATEGORY};
+        Cursor cursor = database.query(CrushersDataBase.TABLE_NAME, columns, CrushersDataBase.COLUMN_CATEGORY + " = '" + category + "'", null, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        return cursor;
+    }
+
+    public List<Result> getSafetyAndBestPracticesCategory() {
+        List<Result> results = new ArrayList<>();
+        database = crushersDataBase.getReadableDatabase();
+        Cursor cursor = this.fetchSafetyAndBestPracticesRecords();
+
+        if (cursor.moveToFirst()) {
+            do {
+                Result result = new Result();
+                result.setScore(cursor.getInt(cursor.getColumnIndexOrThrow(CrushersDataBase.COLUMN_SCORE)));
+                result.setNumOfCorrectAnswers(cursor.getInt(cursor.getColumnIndexOrThrow(CrushersDataBase.COLUMN_CORRECT_ANSWERS)));
+                result.setNumOfWrongAnswers(cursor.getInt(cursor.getColumnIndexOrThrow(CrushersDataBase.COLUMN_WRONG_ANSWERS)));
+                result.setCategory(cursor.getString(cursor.getColumnIndexOrThrow(CrushersDataBase.COLUMN_CATEGORY)));
+                results.add(result);
+            } while (cursor.moveToNext());
+        }
+
+        return results;
+    }
+
+    private Cursor fetchSafetyAndBestPracticesRecords() {
+        String category = "Safety and Best Practices";
+        String[] columns = new String[] {CrushersDataBase.COLUMN_ID, CrushersDataBase.COLUMN_SCORE, CrushersDataBase.COLUMN_CORRECT_ANSWERS, CrushersDataBase.COLUMN_WRONG_ANSWERS, CrushersDataBase.COLUMN_CATEGORY};
+        Cursor cursor = database.query(CrushersDataBase.TABLE_NAME, columns, CrushersDataBase.COLUMN_CATEGORY + " = '" + category + "'", null, null, null, null);
 
         if (cursor != null)
             cursor.moveToFirst();
