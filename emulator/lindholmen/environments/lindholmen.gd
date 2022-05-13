0b77e9fe3c2fc7b1d7453fd7eb1a71d52bd2b5c7
GeorgZs <georg.zsolnai123@gmail.com>
2022-05-13 17:31:13 +0200

    #46 Add event handler for spawn locations

--- emulator/lindholmen/environments/lindholmen.gd
+++ emulator/lindholmen/environments/lindholmen.gd
@@ -1,22 +1,9 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func init_cam_pos() -> Transform:
 	return $CamPosition.global_transform
 	
+## Here instead of $VehicleSpawn.global_transform --> spawnLocation1.instance()
+#### For this to work: couple the spawnLocation1 node to the env
 func get_spawn_position(hint: String) -> Transform:
 	return $VehicleSpawn.global_transform
--- emulator/lindholmen/mods/lindholmen.gd
+++ emulator/lindholmen/mods/lindholmen.gd
@@ -4,3 +4,12 @@ var mod_name: String = "Lindholmen"
 
 func init(global) -> void:
 	global.register_environment("Lindholmen", load("res://environments/lindholmen.tscn"))
+
+#Input handler which checks what spawn point was selected
+func _input(event):
+	## Get the instance of the car and set it to the spawn point
+	if event.is_action_pressed("spawn location 1"):
+		print("Changing Spawn point...")
+		#$VehicleSpawn.set(spawnLocation1.instance())
+	else:
+		print("No spawn location found")
