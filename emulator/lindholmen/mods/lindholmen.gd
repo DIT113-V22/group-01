c869dd42ba1ae9755549c8605f66ffc3ef66cb5d
GeorgZs <georg.zsolnai123@gmail.com>
2022-05-13 19:13:54 +0200

    #46 Add mqtt support - sources cited in document

--- emulator/lindholmen/mods/lindholmen.gd
+++ emulator/lindholmen/mods/lindholmen.gd
@@ -3,13 +3,34 @@ extends Reference
 var mod_name: String = "Lindholmen"
 
 func init(global) -> void:
+	Mqtt.connect_to_server()
+	Mqtt.subscribe("/smartcar/heartbeat")
+	Mqtt.subscribe("/smartcar/spawnLocation")
+	print(Mqtt.check_msg())
 	global.register_environment("Lindholmen", load("res://environments/lindholmen.tscn"))
 
 #Input handler which checks what spawn point was selected
 func _input(event):
+	var spawn_location = Mqtt.check_msg()
+	#set spawn point
+	match spawn_location:
+		"spawn location 1":
+			#set vehicle spawn
+			print("opening spawn 1")
+		"spawn location 2":
+			#set vehicle spawn
+			print("opening spawn 2")
+		"spawn location 3":
+			#set vehicle spawn
+			print("opening spawn 3")
+		_:
+			print("error")
+	
+	
 	## Get the instance of the car and set it to the spawn point
-	if event.is_action_pressed("spawn location 1"):
-		print("Changing Spawn point...")
-		#$VehicleSpawn.set(spawnLocation1.instance())
-	else:
-		print("No spawn location found")
+	
+	#if event.is_action_pressed("spawn location 1"):
+	#	print("Changing Spawn point...")
+	#	#$VehicleSpawn.set(spawnLocation1.instance())
+	#else:
+	#	print("No spawn location found")
