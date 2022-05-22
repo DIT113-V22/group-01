6de2eb33b02de0983e665977f3e61cc57b785d4b
Lance J <lancear153@gmail.com>
2022-05-22 18:09:14 +0200

    #84 Fix camera and Lindholmen environment

--- emulator/lindholmen/environments/lindholmen.gd
+++ emulator/lindholmen/environments/lindholmen.gd
@@ -4,11 +4,9 @@ extends Spatial
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	get_viewport().get_camera().far = 2500
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
